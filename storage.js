40681f332a8d944a5e8a0056f36894da59f43222
Tatsh <ddrtist@gmail.com>
2012-03-27 19:34:25 -0500

    Add clear() method if needed to globalStorage; JSHint

--- storage.js
+++ storage.js
@@ -1,20 +1,36 @@
+/*jshint sub:true */
+// kate: tabs
 /*
 Copyright (c) 2011 Wojo Design
 Dual licensed under the MIT or GPL licenses.
 */
 (function(){
-	var window = this;
 	// check to see if we have localStorage or not
-	if( !window.localStorage ){		
+	if( !window.localStorage ){
 
 		// globalStorage
 		// non-standard: Firefox 2+
 		// https://developer.mozilla.org/en/dom/storage#globalStorage
 		if ( window.globalStorage ) {
 			// try/catch for file protocol in Firefox
+			// globalStorage is not the same as localStorage however
+			// NOTE globalStorage will return null instead of undefined on keys that don't exist
+			// NOTE globalStorage will return an object with a 'value' key instead of the value directly
 			try {
-				window.localStorage = window.globalStorage;
+				var store = window.globalStorage[location.hostname];
+				// Add the clear() method if needed, globalStorage doesn't normally have it
+				if (!window.localStorage.clear) {
+					window.localStorage.clear = function () {
+						for (var key in store) {
+							if (store.hasOwnProperty(key)) {
+								store.removeItem(key);
+							}
+						}
+					};
+				}
+				window.localStorage = window.globalStorage[location.hostname];
 			} catch( e ) {}
+
 			return;
 		}
 
@@ -33,12 +49,12 @@ Dual licensed under the MIT or GPL licenses.
 				"setItem":function( key , value ){
 					div.load( attrKey );
 					key = cleanKey(key );
-				
+
 					if( !div.getAttribute( key ) ){
 						this.length++;
 					}
 					div.setAttribute( key , value );
-				
+
 					div.save( attrKey );
 				},
 				"getItem":function( key ){
@@ -51,41 +67,41 @@ Dual licensed under the MIT or GPL licenses.
 					div.load( attrKey );
 					key = cleanKey(key );
 					div.removeAttribute( key );
-				
+
 					div.save( attrKey );
 					this.length--;
 					if( this.length < 0){
 						this.length=0;
 					}
 				},
-			
+
 				"clear":function(){
 					div.load( attrKey );
 					var i = 0;
-					while ( attr = div.XMLDocument.documentElement.attributes[ i++ ] ) {
+					while (( attr = div.XMLDocument.documentElement.attributes[ i++ ] )) {
 						div.removeAttribute( attr.name );
 					}
 					div.save( attrKey );
 					this.length=0;
-				}, 
-			
+				},
+
 				"key":function( key ){
 					div.load( attrKey );
 					return div.XMLDocument.documentElement.attributes[ key ];
 				}
 
 			},
-		
+
 			// convert invalid characters to dashes
 			// http://www.w3.org/TR/REC-xml/#NT-Name
 			// simplified to assume the starting character is valid
 			cleanKey = function( key ){
-				return key.replace( /[^-._0-9A-Za-z\xb7\xc0-\xd6\xd8-\xf6\xf8-\u037d\u37f-\u1fff\u200c-\u200d\u203f\u2040\u2070-\u218f]/g, "-" );
+				return key.replace( /[^\-._0-9A-Za-z\xb7\xc0-\xd6\xd8-\xf6\xf8-\u037d\u37f-\u1fff\u200c-\u200d\u203f\u2040\u2070-\u218f]/g, "-" );
 			};
-		
-	
+
+
 			div.load( attrKey );
 			localStorage["length"] = div.XMLDocument.documentElement.attributes.length;
-		} 
-	} 
+		}
+	}
 })();
