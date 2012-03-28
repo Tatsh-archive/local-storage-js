8d7fa062cac2fb3c48c2cc6dca0d6a309e7b1272
Tatsh <ddrtist@gmail.com>
2012-03-27 21:05:50 -0500

    Finish externs

--- extern-userdata.js
+++ extern-userdata.js
@@ -15,3 +15,13 @@ IEUserData.prototype.save = function (storeName) {};
  * @see http://msdn.microsoft.com/en-us/library/ms531328(v=vs.85).aspx
  */
 IEUserData.prototype.XMLDocument;
+/**
+ * @param {string} storeName
+ * @see http://msdn.microsoft.com/en-us/library/ms531395(v=vs.85).aspx
+ */
+IEUserData.prototype.load = function (storeName) {};
+/**
+ * @type string
+ * @see http://msdn.microsoft.com/en-us/library/ms531095(v=vs.85).aspx
+ */
+IEUserData.prototype.expires;
